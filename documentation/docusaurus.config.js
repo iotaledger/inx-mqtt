90bde6f6769e88aafa7ddb701c5bbc9a9a81bd47
Alexander Sporn <github@alexsporn.de>
2022-07-25 13:00:23 +0200

    - Added documentation

---
+++ documentation/docusaurus.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        [
+            '@docusaurus/plugin-content-docs',
+            {
+                id: 'inx-mqtt-develop',
+                path: path.resolve(__dirname, 'docs'),
+                routeBasePath: 'inx-mqtt',
+                sidebarPath: path.resolve(__dirname, 'sidebars.js'),
+                editUrl: 'https://github.com/gohornet/inx-mqtt/edit/develop/documentation',
+                versions: {
+                    current: {
+                        label: 'Develop',
+                        path: 'develop',
+                        badge: true
+                    },
+                },
+            }
+        ],
+    ],
+    staticDirectories: [path.resolve(__dirname, 'static')],
+};
