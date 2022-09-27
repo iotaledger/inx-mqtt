43bfda98070415544fb97a8cff3c19349e6ccf59
Dr-Electron <dr-electr0n@protonmail.com>
2022-09-27 22:13:58 +0200

    Remove docs version info

--- documentation/docusaurus.config.js
+++ documentation/docusaurus.config.js
@@ -5,18 +5,11 @@ module.exports = {
         [
             '@docusaurus/plugin-content-docs',
             {
-                id: 'inx-mqtt-develop',
+                id: 'inx-mqtt',
                 path: path.resolve(__dirname, 'docs'),
                 routeBasePath: 'inx-mqtt',
                 sidebarPath: path.resolve(__dirname, 'sidebars.js'),
                 editUrl: 'https://github.com/gohornet/inx-mqtt/edit/develop/documentation/docs',
-                versions: {
-                    current: {
-                        label: 'Develop',
-                        path: 'develop',
-                        badge: true
-                    },
-                },
             }
         ],
     ],
